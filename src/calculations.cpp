54085286ac7a48e5c8bb032ecf9bd8defd6232ec
Agnar22 <agnar.bjornstad@gmail.com>
2020-06-12 18:13:50 +0200

    Create functions related to computing the number of suit permutations for a given hand.

--- src/calculations.cpp
+++ src/calculations.cpp
@@ -38,3 +38,53 @@ unsigned long long compress_hand_lossless(unsigned long long hand) {
     return compressed_hand;
 }
 
+namespace calculations {
+
+    std::unordered_map< unsigned long long, int > suit_permutations;
+
+    void calculate_suit_permutations(unsigned long long current_hand, int upper_bound, int max_cards) {
+        if (max_cards == 0) {
+            unsigned long long compressed_hand = compress_hand_lossless(current_hand);
+            if (calculations::suit_permutations.find(compressed_hand) != calculations::suit_permutations.end())
+                calculations::suit_permutations[compressed_hand]++;
+            else
+                calculations::suit_permutations[compressed_hand] = 1;
+        } else {
+            for (int x = 0; x < upper_bound; x++) {
+                if (current_hand & 1ULL<<x)
+                    continue;
+                calculations::calculate_suit_permutations(current_hand | 1ULL<<x, x, max_cards - 1);
+            }
+        }
+    }
+
+    void load_suit_permutations() {
+        std::string file_name = "../files/suit_permutations.txt";
+
+        if (!calculations::suit_permutations.empty())
+            return;
+
+        if (std::experimental::filesystem::exists(file_name)) {
+            std::ifstream ifs(file_name);
+            boost::archive::text_iarchive ia(ifs);
+            ia >> calculations::suit_permutations;
+            ifs.close();
+        } else {
+            for (int max_cards = 1; max_cards <= 7; ++max_cards)
+                calculations::calculate_suit_permutations(0ULL, 52, max_cards);
+            std::ofstream ofs(file_name);
+            boost::archive::text_oarchive oa(ofs);
+            oa << calculations::suit_permutations;
+            ofs.close();
+        }
+    }
+
+    int num_suit_permutations(unsigned long long hand) {
+        if (calculations::suit_permutations.empty())
+            calculations::load_suit_permutations();
+
+        return calculations::suit_permutations[compress_hand_lossless(hand)];
+    }
+
+} // namespace calculations
+
--- src/calculations.h
+++ src/calculations.h
@@ -1,9 +1,26 @@
 #ifndef CALCULATIONS_H
 #define CALCULATIONS_H
 
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/unordered_map.hpp>
+#include <boost/serialization/vector.hpp>
+#include <experimental/filesystem>
+#include <fstream>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 
 unsigned long long compress_hand_lossless(unsigned long long);
+
+namespace calculations {
+
+    extern std::unordered_map< unsigned long long, int > suit_permutations;
+    extern std::unordered_map< int, std::vector<int> > hand_frequencies;
+    void load_suit_permutations();
+    int num_suit_permutations(unsigned long long);
+
+}
 
 #endif
