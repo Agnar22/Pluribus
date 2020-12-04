4bdc58a06997a819f0498afecc4801110763258c
Agnar22 <agnar.bjornstad@gmail.com>
2020-12-04 20:00:28 +0100

    Do not use pure virtual functions.

--- src/game.h
+++ src/game.h
@@ -7,23 +7,23 @@
 
 class Game {
     public:
-        virtual std::set<std::string> get_encountered_infosets(int) = 0;
-        virtual void execute(std::string) = 0;
-        virtual void undo() = 0;
-        virtual bool is_finished() = 0;
-        virtual bool is_player_in_hand(int) = 0;
-        virtual bool is_chance_node() = 0;
-        virtual int betting_round() = 0;
-        virtual std::string sample_action() = 0;
-        virtual bool is_player_to_move(int) = 0;
-        virtual int get_player_to_move() = 0;
-        virtual std::vector<std::string> get_actions() = 0;
-        virtual std::string get_random_action() = 0;
-        virtual std::string get_infoset(int) = 0;
-        virtual std::string get_current_infoset() = 0;
-        virtual float get_outcome_for_player(int) = 0;
-        virtual std::vector<std::string> get_actions_from_infoset(std::string) = 0;
-        virtual std::vector<int> get_players() = 0;
+        virtual std::set<std::string> get_encountered_infosets(int) {};
+        virtual void execute(std::string) {};
+        virtual void undo() {};
+        virtual bool is_finished() {};
+        virtual bool is_player_in_hand(int) {};
+        virtual bool is_chance_node() {};
+        virtual int betting_round() {};
+        virtual std::string sample_action() {};
+        virtual bool is_player_to_move(int) {};
+        virtual int get_player_to_move() {};
+        virtual std::vector<std::string> get_actions() {};
+        virtual std::string get_random_action() {};
+        virtual std::string get_infoset(int) {};
+        virtual std::string get_current_infoset() {};
+        virtual float get_outcome_for_player(int) {};
+        virtual std::vector<std::string> get_actions_from_infoset(std::string) {};
+        virtual std::vector<int> get_players() {};
 };
 
 #endif
