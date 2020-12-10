9f58c50e6599b472959175bd92099315c77710f3
Agnar22 <agnar.bjornstad@gmail.com>
2020-12-10 19:28:40 +0100

    Remove debug info.

--- src/tree.h
+++ src/tree.h
@@ -8,30 +8,13 @@
 typedef std::underlying_type<Cards>::type cards_type;
 typedef std::underlying_type<Move>::type move_type;
 
-int num_nodes=0;
-
 struct Tree {
     int num;
     Tree* children[MAX_MOVES];
     float infoset_regret[MAX_CARDS][MAX_MOVES];
     float infoset_strategy[MAX_CARDS][MAX_MOVES];
 
-    Tree() : children() {
-        //std::cout << "Initializing tree" << std::endl;
-        num = num_nodes++;
-    }
-
-    ~Tree() {
-        //std::cout << "Destructing tree" << std::endl;
-        //for (int x=0; x<MAX_MOVES; ++x) {
-            //std::cout << "iterating through children" << x << std::endl;
-            //if (children[x] != nullptr) {
-                //std::cout << "deleting child" << std::endl;
-                //delete children[x];
-                //std::cout << "deleted child" << std::endl;
-            //}
-        //}
-    }
+    Tree() : children() {}
 
     int delete_tree() {
         int num_deleted = delete_tree(0);
@@ -40,10 +23,8 @@ struct Tree {
     }
 
     int delete_tree(int depth){
-        //std::cout << "Deleting children for " << num << std::endl;
         int deleted_children = 0;
         for (Tree* child:children) {
-            //std::cout << "Deleted child for " << num << std::endl;
             if (child != nullptr)
                 deleted_children += child->delete_tree(depth+1);
             delete child;
@@ -51,7 +32,6 @@ struct Tree {
         }
         return deleted_children;
     }
-
 
     inline float get_regret(Cards card, Move move) {
         return infoset_regret[static_cast<cards_type>(card)][static_cast<move_type>(move)];
@@ -70,22 +50,10 @@ struct Tree {
     }
 
     Tree* get_child(int move) {
-        if (children[move] == nullptr) {
-            children[move] = new Tree();
-            //std::cout << "Created ";
-            //std::cout << num << std::endl;
-        } else {
-            //std::cout << "not created" << std::endl;
-         }
-        return children[move];
-    }
-/*
-    Tree* operator[](int move) {
-        if (children[move] != nullptr)
+        if (children[move] == nullptr)
             children[move] = new Tree();
         return children[move];
     }
-*/
 };
 
 #endif
